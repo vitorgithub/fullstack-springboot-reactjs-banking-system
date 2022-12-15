33c9820367f1833515bf6f50c7aa6e2bb82f6fa8
Vitor Luiz <vitorgithub@gmail.com>
2022-12-14 23:55:59 -0300

    first commit

---
+++ backend/src/test/java/br/com/example/sistemabancario/SistemaBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.sistemabancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
